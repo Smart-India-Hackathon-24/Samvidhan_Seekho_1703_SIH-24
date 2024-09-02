df5b98b4f0fa289c4bd5172cceb1b864c94019b6
Khushil-21 <khushilshah2105@gmail.com>
2024-09-02 13:00:57 +0530

    Backend: removed schemas

--- backend/Database/DatabaseFunctions.py
+++ backend/Database/DatabaseFunctions.py
@@ -1,12 +1,7 @@
-from Database.Schemas.ConstitutionSectionSchema import validate_section_schema
 from Database.utils import get_collection
 
 
 def addConstitutionSection(part, articles):
     collection = get_collection("ConstitutionalSections")
-    if validate_section_schema(part, articles):
-        collection.insert_one(part, articles)
-        print("Constitution section added successfully")
-    else:
-        print("Constitution section was not added")
-        return "Invalid schema"
+    collection.insert_one(part, articles)
+    print("Constitution section added successfully")
