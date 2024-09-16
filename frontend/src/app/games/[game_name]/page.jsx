fffaeb9889196cf07fc000b628cd345baf750d0f
Khushil-21 <khushilshah2105@gmail.com>
2024-09-16 14:02:27 +0530

    Cross word route , game dynamic route

---
+++ frontend/src/app/games/[game_name]/page.jsx
@@ -0,0 +1,28 @@
+import { GameCategoriesData } from "@/components/samvidhan/Game/Constants";
+import EachGamePage from "@/components/samvidhan/Game/EachGame/EachGamePage";
+
+export async function generateMetadata({ params }) {
+	return {
+		title: `Game | ${params.game_name
+			.split("-")
+			.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+			.join(" ")}`,
+	};
+}
+
+export async function generateStaticParams() {
+	return GameCategoriesData.flatMap((category) =>
+		category.games.map((game) => ({
+			game_name: game.name,
+		}))
+	);
+}
+
+export default function page({ params }) {
+	const { game_name } = params;
+	return (
+		<div>
+			<EachGamePage game_name={game_name.toLowerCase().replace("-", " ")} />
+		</div>
+	);
+}
